43e7747e8d15a21a9ad7ee7af0cef73834e2caf9
Logan Bek <loganbek@gmail.com>
2021-06-09 03:49:05 +0000

    rainbow page header

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -5,9 +5,8 @@ import Container from '../layout/Container'
 
 const Outer = styled(Box)<{ background?: string }>`
   background: ${({ theme, background }) => background || theme.colors.gradients.bubblegum};
-  z-index: -1;
-  background: linear-gradient(to right bottom, rgb(77, 84, 209), rgb(165, 28, 123) 50%, rgb(238, 74, 55) 100%) center center;
-  animation: 2s linear 0s infinite normal none running;
+  background: linear-gradient(45deg, rgb(255, 0, 0) 0%, rgb(255, 154, 0) 10%, rgb(208, 222, 33) 20%, rgb(79, 220, 74) 30%, rgb(63, 218, 216) 40%, rgb(47, 201, 226) 50%, rgb(28, 127, 238) 60%, rgb(95, 21, 242) 70%, rgb(186, 12, 248) 80%, rgb(251, 7, 217) 90%, rgb(255, 0, 0) 100%) 0% 0% / 300% 300%;
+  animation: 2s linear 0s infinite normal none backwards;
 `
 
 const Inner = styled(Container)`
