2a2c052c44b3a5bb647ee7b030a41eea9be0549c
Adam Bertram <adbertram@gmail.com>
2021-09-16 16:15:16 -0500

    renamed stake button to Stake/harvest on farms page

--- src/views/Farms/components/FarmCard/StakeAction.tsx
+++ src/views/Farms/components/FarmCard/StakeAction.tsx
@@ -80,7 +80,7 @@ const StakeAction: React.FC<FarmCardActionsProps> = ({
         onClick={onPresentDeposit}
         disabled={['history', 'archived'].some((item) => location.pathname.includes(item))}
       >
-        {t('Stake')}
+        {t('Stake/Harvest')}
       </Button>
     ) : (
       <IconButtonWrapper>
