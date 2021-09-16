14d05463c51565ab7b7d98148ff8d2f68a88d41d
Kenneth Ashley <ken.u.diggitt@gmail.com>
2021-09-16 12:13:28 -0600

    Revert "update useHarvest and callHelpers"
    This reverts commit 956330f802c2adcae64508e034c8e35a6b1c7eb8.

--- src/hooks/useHarvest.ts
+++ src/hooks/useHarvest.ts
@@ -7,65 +7,65 @@ import { useMasterchef, useSousChef } from './useContract'
 
 export const useHarvest = (farmPid: number) => {
   const dispatch = useAppDispatch()
-  const { account, library } = useWeb3React()
+  const { account } = useWeb3React()
   const masterChefContract = useMasterchef()
 
   const handleHarvest = useCallback(async () => {
-    const txHash = await harvest(masterChefContract, farmPid, account, library)
+    const txHash = await harvest(masterChefContract, farmPid, account)
     dispatch(fetchFarmUserDataAsync(account))
     return txHash
-  }, [account, dispatch, farmPid, masterChefContract, library])
+  }, [account, dispatch, farmPid, masterChefContract])
 
   return { onReward: handleHarvest }
 }
 
 export const useAllHarvest = (farmPids: number[]) => {
-  const { account, library } = useWeb3React()
+  const { account } = useWeb3React()
   const masterChefContract = useMasterchef()
 
   const handleHarvest = useCallback(async () => {
     const harvestPromises = farmPids.reduce((accum, pid) => {
-      return [...accum, harvest(masterChefContract, pid, account, library)]
+      return [...accum, harvest(masterChefContract, pid, account)]
     }, [])
 
     return Promise.all(harvestPromises)
-  }, [account, farmPids, masterChefContract, library])
+  }, [account, farmPids, masterChefContract])
 
   return { onReward: handleHarvest }
 }
 
 export const useSousHarvest = (sousId, isUsingBnb = false) => {
   const dispatch = useAppDispatch()
-  const { account, library } = useWeb3React()
+  const { account } = useWeb3React()
   const sousChefContract = useSousChef(sousId)
   const masterChefContract = useMasterchef()
 
   const handleHarvest = useCallback(async () => {
     if (sousId === 0) {
-      await harvest(masterChefContract, 0, account, library)
+      await harvest(masterChefContract, 0, account)
     } else if (isUsingBnb) {
-      await soushHarvestBnb(sousChefContract, account, library)
+      await soushHarvestBnb(sousChefContract, account)
     } else {
-      await soushHarvest(sousChefContract, account, library)
+      await soushHarvest(sousChefContract, account)
     }
     dispatch(updateUserPendingReward(sousId, account))
     dispatch(updateUserBalance(sousId, account))
-  }, [account, dispatch, isUsingBnb, masterChefContract, sousChefContract, sousId, library])
+  }, [account, dispatch, isUsingBnb, masterChefContract, sousChefContract, sousId])
 
   return { onReward: handleHarvest }
 }
 
 export const useMasterChefHarvest = (sousId, pid: number) => {
   const dispatch = useAppDispatch()
-  const { account, library } = useWeb3React()
+  const { account } = useWeb3React()
   const masterChefContract = useMasterchef()
 //  const sousChefContract = useSousChef(sousId)
 
   const handleHarvest = useCallback(async () => {
-  await masterchefHarvest(masterChefContract, pid, account, library)
+  await masterchefHarvest(masterChefContract, pid, account)
   dispatch(updateUserPendingReward(sousId, account))
   dispatch(updateUserBalance(sousId, account))
-}, [account, dispatch, masterChefContract, pid, sousId, library])
+}, [account, dispatch, masterChefContract, pid, sousId])
 
   return { onReward: handleHarvest }
 }
--- src/utils/callHelpers.ts
+++ src/utils/callHelpers.ts
@@ -1,10 +1,7 @@
-import { getWeb3NoAccount } from 'utils/web3'
-import Web3 from 'web3'
 import BigNumber from 'bignumber.js'
 import { DEFAULT_TOKEN_DECIMAL, KOGE_TOKEN_DECIMAL } from 'config'
 import { ethers } from 'ethers'
 import { BIG_TEN, BIG_ZERO } from './bigNumber'
-
 
 export const approve = async (lpContract, masterChefContract, account) => {
   return lpContract.methods
@@ -19,29 +16,25 @@ export const jarApprove = async (lpContract, jarContractAddress, account) => {
 }
 
 
-export const stake = async (masterChefContract, pid, amount, account, library) => {
-  const [gasLimit] = await estimateGas(masterChefContract.methods.deposit(pid, new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString()), account, library)
-
+export const stake = async (masterChefContract, pid, amount, account) => {
   return masterChefContract.methods
     .deposit(pid, new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString())
-    .send({ from: account, gas: gasLimit })
+    .send({ from: account, gas: 400000 })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
 }
 
-export const stakeKoge = async (masterChefContract, pid, amount, account, library) => {
-  const [gasLimit] = await estimateGas(masterChefContract.methods.deposit(pid, new BigNumber(amount).times(KOGE_TOKEN_DECIMAL).toString()), account, library)
-
+export const stakeKoge = async (masterChefContract, pid, amount, account) => {
   return masterChefContract.methods
     .deposit(pid, new BigNumber(amount).times(KOGE_TOKEN_DECIMAL).toString())
-    .send({ from: account, gas: gasLimit })
+    .send({ from: account, gas: 400000 })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
 }
 
-export const depositJar = async (jarContract, amount, account, library) => {
+export const depositJar = async (jarContract, amount, account) => {
   let decimals = DEFAULT_TOKEN_DECIMAL
   if (jarContract.options.address==="0x992Ae1912CE6b608E0c0d2BF66259ab1aE62A657"){
     decimals = KOGE_TOKEN_DECIMAL
@@ -52,13 +45,13 @@ export const depositJar = async (jarContract, amount, account, library) => {
   if (jarContract.options.address==="0x5AB24bA8183dA4D0E050fD00257dE291CDD17a89"){
     decimals = new BigNumber(10**6)
   }
-  if (jarContract.options.address==="0x1B247E74d5c2D6CeF12C9BEc8115282CbC45c8B5" || jarContract.options.address==="0x0eB72cd5f9911C307a0E3B07426a7FDeb4588600"  || jarContract.options.address==="0x89030969DB2B51897D7f4C789b7C9d3c2ef05083" || jarContract.options.address==="0xb9F9E41ac77A6B6E90e7EC26D9CDcEdB194F508A"){
+  if (jarContract.options.address==="0x1B247E74d5c2D6CeF12C9BEc8115282CbC45c8B5"){
     decimals = new BigNumber(10**6)
   }
-  if (jarContract.options.address==="0x58c12402428ca79Da43Bf14B70CbC59DF5Dfe61a" || jarContract.options.address==="0xAFBFB2Bc9C0C00781993B203f43605966C8cD637" || jarContract.options.address==="0x2C61069B2e429F132CeF57D28aC58F4fA91B1dE1"){
+  if (jarContract.options.address==="0x58c12402428ca79Da43Bf14B70CbC59DF5Dfe61a"){
     decimals = new BigNumber(10**8)
   }
-
+  const gasLimit = 3000000
 /*  if (jarContract.options.address==="0xA2C2473ccE81aF668f25C8d798f7A1C0F9172Cb9"){
     gasLimit = 3000000
   }
@@ -83,60 +76,51 @@ export const depositJar = async (jarContract, amount, account, library) => {
   if (jarContract.options.address==="0x52fd74517b9728E4a8c6A44a6D5efeECB49e2aC5"){
     gasLimit = 3000000
   } */
-  const [gasLimit, gasPrice] = await estimateGas(jarContract.methods.deposit(new BigNumber(amount).times(decimals).toString()), account, library)
-
   return jarContract.methods
     .deposit(new BigNumber(amount).times(decimals).toString())
-    .send({ from: account, gas: gasLimit, gasPrice })
+    .send({ from: account, gas: gasLimit, gasPrice:'5000000000' })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
 }
 
-export const sousStake = async (sousChefContract, amount, decimals = 18, account, library) => {
-  const [gasLimit, gasPrice] = await estimateGas(sousChefContract.methods.deposit(new BigNumber(amount).times(BIG_TEN.pow(decimals)).toString()), account, library)
-
+export const sousStake = async (sousChefContract, amount, decimals = 18, account) => {
   return sousChefContract.methods
     .deposit(new BigNumber(amount).times(BIG_TEN.pow(decimals)).toString())
-    .send({ from: account, gas: gasLimit, gasPrice })
+    .send({ from: account, gas: 400000, gasPrice:'5000000000' })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
 }
 
-export const sousStakeBnb = async (sousChefContract, amount, account, library) => {
-  const [gasLimit, gasPrice] = await estimateGas(sousChefContract.methods.deposit(), account, library)
-
+export const sousStakeBnb = async (sousChefContract, amount, account) => {
   return sousChefContract.methods
     .deposit()
-    .send({ from: account, gas: gasLimit, gasPrice, value: new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString() })
+    .send({ from: account, gas: 400000, gasPrice:'5000000000', value: new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString() })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
 }
 
-export const unstake = async (masterChefContract, pid, amount, account, library) => {
+export const unstake = async (masterChefContract, pid, amount, account) => {
   if (pid === 0) {
-    const [gasLimit, gasPrice] = await estimateGas(masterChefContract.methods.leaveStaking(new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString()), account, library)
-
     return masterChefContract.methods
       .leaveStaking(new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString())
-      .send({ from: account, gas: gasLimit, gasPrice })
+      .send({ from: account, gas: 400000, gasPrice:'5000000000' })
       .on('transactionHash', (tx) => {
         return tx.transactionHash
       })
   }
-  const [gasLimit, gasPrice] = await estimateGas(masterChefContract.methods.withdraw(pid, new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString()), account, library)
 
   return masterChefContract.methods
     .withdraw(pid, new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString())
-    .send({ from: account, gas: gasLimit, gasPrice })
+    .send({ from: account, gas: 400000, gasPrice:'5000000000' })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
 }
 
-export const withdrawalJar = async (jarContract, amount, account, library) => {
+export const withdrawalJar = async (jarContract, amount, account) => {
   let decimals = DEFAULT_TOKEN_DECIMAL
   if (jarContract.options.address==="0x992Ae1912CE6b608E0c0d2BF66259ab1aE62A657"){
     decimals = KOGE_TOKEN_DECIMAL
@@ -147,40 +131,34 @@ export const withdrawalJar = async (jarContract, amount, account, library) => {
   if (jarContract.options.address==="0x5AB24bA8183dA4D0E050fD00257dE291CDD17a89"){
     decimals = new BigNumber(10**6)
   }
-  if (jarContract.options.address==="0x1B247E74d5c2D6CeF12C9BEc8115282CbC45c8B5" || jarContract.options.address==="0x0eB72cd5f9911C307a0E3B07426a7FDeb4588600"  || jarContract.options.address==="0x89030969DB2B51897D7f4C789b7C9d3c2ef05083" || jarContract.options.address==="0xb9F9E41ac77A6B6E90e7EC26D9CDcEdB194F508A"){
-    decimals = new BigNumber(10**6)
-  }
-  if (jarContract.options.address==="0x58c12402428ca79Da43Bf14B70CbC59DF5Dfe61a" || jarContract.options.address==="0xAFBFB2Bc9C0C00781993B203f43605966C8cD637" || jarContract.options.address==="0x2C61069B2e429F132CeF57D28aC58F4fA91B1dE1"){
+  if (jarContract.options.address==="0x58c12402428ca79Da43Bf14B70CbC59DF5Dfe61a"){
     decimals = new BigNumber(10**8)
   }
-
-  const [gasLimit, gasPrice] = await estimateGas(jarContract.methods.withdraw(new BigNumber(amount).times(decimals).toString()), account, library)
+  if (jarContract.options.address==="0x1B247E74d5c2D6CeF12C9BEc8115282CbC45c8B5"){
+    decimals = new BigNumber(10**6)
+  }
   return jarContract.methods
     .withdraw(new BigNumber(amount).times(decimals).toString())
-    .send({ from: account, gas: gasLimit, gasPrice })
+    .send({ from: account, gas: 800000, gasPrice:'5000000000' })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
 }
 
 
-export const sousUnstake = async (sousChefContract, amount, decimals, account, library) => {
-  const [gasLimit, gasPrice] = await estimateGas(sousChefContract.methods.withdraw(new BigNumber(amount).times(BIG_TEN.pow(decimals)).toString()), account, library)
-
+export const sousUnstake = async (sousChefContract, amount, decimals, account) => {
   return sousChefContract.methods
     .withdraw(new BigNumber(amount).times(BIG_TEN.pow(decimals)).toString())
-    .send({ from: account, gas: gasLimit, gasPrice })
+    .send({ from: account, gas: 800000, gasPrice:'5000000000' })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
 }
 
-export const masterChefUnstake = async (masterChefContract, pid, amount, decimals, account, library) => {
-  const [gasLimit, gasPrice] = await estimateGas(masterChefContract.methods.withdraw(pid, new BigNumber(amount).times(BIG_TEN.pow(decimals)).toString()), account, library)
-
+export const masterChefUnstake = async (masterChefContract, pid, amount, decimals, account) => {
   return masterChefContract.methods
     .withdraw(pid, new BigNumber(amount).times(BIG_TEN.pow(decimals)).toString())
-    .send({ from: account, gas: gasLimit, gasPrice })
+    .send({ from: account, gas: 800000, gasPrice:'5000000000' })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
@@ -205,76 +183,48 @@ export const masterChefEmergencyUnstake = async (masterChefContract, pid, accoun
 }
 
 
-export const harvest = async (masterChefContract, pid, account, library) => {
+export const harvest = async (masterChefContract, pid, account) => {
   if (pid === 0) {
-    const [gasLimit, gasPrice] = await estimateGas(masterChefContract.methods.leaveStaking('0'), account, library)
-
     return masterChefContract.methods
       .leaveStaking('0')
-      .send({ from: account, gas: gasLimit, gasPrice })
+      .send({ from: account, gas: 400000, gasPrice:'5000000000' })
       .on('transactionHash', (tx) => {
         return tx.transactionHash
       })
   }
-  const [gasLimit, gasPrice] = await estimateGas(masterChefContract.methods.deposit(pid, '0'), account, library)
 
   return masterChefContract.methods
     .deposit(pid, '0')
-    .send({ from: account, gas: gasLimit, gasPrice })
+    .send({ from: account, gas: 400000, gasPrice:'5000000000' })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
 }
 
-export const soushHarvest = async (sousChefContract, account, library) => {
-  const [gasLimit, gasPrice] = await estimateGas(sousChefContract.methods.deposit('0'), account, library)
-
+export const soushHarvest = async (sousChefContract, account) => {
   return sousChefContract.methods
     .deposit('0')
-    .send({ from: account, gas: gasLimit, gasPrice })
+    .send({ from: account, gas: 400000, gasPrice:'5000000000' })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
 }
 
-export const masterchefHarvest = async (masterChefContract, pid, account, library) => {
-  const [gasLimit, gasPrice] = await estimateGas(masterChefContract.methods.deposit(pid, '0'), account, library)
-
+export const masterchefHarvest = async (masterChefContract, pid, account) => {
   return masterChefContract.methods
     .deposit(pid, '0')
-    .send({ from: account, gas: gasLimit, gasPrice })
+    .send({ from: account, gas: 400000, gasPrice:'5000000000' })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
 }
 
 
-export const soushHarvestBnb = async (sousChefContract, account, library) => {
-  const [gasLimit, gasPrice] = await estimateGas(sousChefContract.methods.deposit(), account, library)
-
+export const soushHarvestBnb = async (sousChefContract, account) => {
   return sousChefContract.methods
     .deposit()
-    .send({ from: account, gas: gasLimit, gasPrice, value: BIG_ZERO })
+    .send({ from: account, gas: 400000, gasPrice:'5000000000', value: BIG_ZERO })
     .on('transactionHash', (tx) => {
       return tx.transactionHash
     })
-}
-
-export const estimateGas = async(callObject, account, library) => {
-  let gasLimit = await callObject.estimateGas({ from: account })
-
-  // estimateGas is not super accurate, so doubling it to be safe
-  gasLimit *= 2
-
-  // Matic has gasLimit of 20M per block, so want to set to 15M if estimate is higher
-  if(gasLimit > 15000000) {
-    gasLimit = 15000000
-  }
-
-  const web3 = library ? new Web3(library) : getWeb3NoAccount()
-
-  const gasPrice = await web3.eth.getGasPrice();
-  const gasPriceEstimate = (parseInt(gasPrice)*2).toString();
-
-  return [gasLimit, gasPriceEstimate]
 }
