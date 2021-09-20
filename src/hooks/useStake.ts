76102680ba8a89549db203e002e9acf634103eba
Adam Bertram <adbertram@gmail.com>
2021-09-20 10:12:02 -0500

    gas estimation

--- src/hooks/useStake.ts
+++ src/hooks/useStake.ts
@@ -8,16 +8,16 @@ import { useMasterchef, useSousChef, useJar } from './useContract'
 
 const useStake = (jarAddress: string) => {
   const dispatch = useAppDispatch()
-  const { account } = useWeb3React()
+  const { account, library } = useWeb3React()
   const jarContract = useJar(jarAddress)
 
   const handleStake = useCallback(
     async (amount: string) => {
-      const txHash = await depositJar(jarContract, amount, account)
+      const txHash = await depositJar(jarContract, amount, account, library)
       dispatch(fetchFarmUserDataAsync(account))
       console.info(txHash)
     },
-    [account, dispatch, jarContract],
+    [account, dispatch, jarContract, library],
   )
 
   return { onStake: handleStake }
@@ -25,23 +25,23 @@ const useStake = (jarAddress: string) => {
 
 export const useSousStake = (sousId: number, isUsingBnb = false) => {
   const dispatch = useAppDispatch()
-  const { account } = useWeb3React()
+  const { account, library } = useWeb3React()
   const masterChefContract = useMasterchef()
   const sousChefContract = useSousChef(sousId)
 
   const handleStake = useCallback(
     async (amount: string, decimals: number) => {
       if (sousId === 0) {
-        await stake(masterChefContract, 0, amount, account)
+        await stake(masterChefContract, 0, amount, account, library)
       } else if (isUsingBnb) {
-        await sousStakeBnb(sousChefContract, amount, account)
+        await sousStakeBnb(sousChefContract, amount, account, library)
       } else {
-        await sousStake(sousChefContract, amount, decimals, account)
+        await sousStake(sousChefContract, amount, decimals, account, library)
       }
       dispatch(updateUserStakedBalance(sousId, account))
       dispatch(updateUserBalance(sousId, account))
     },
-    [account, dispatch, isUsingBnb, masterChefContract, sousChefContract, sousId],
+    [account, dispatch, isUsingBnb, masterChefContract, sousChefContract, sousId, library],
   )
 
   return { onStake: handleStake }
@@ -50,21 +50,21 @@ export const useSousStake = (sousId: number, isUsingBnb = false) => {
 
 export const useMasterChefStake = (sousId:number, pid: number) => {
   const dispatch = useAppDispatch()
-  const { account } = useWeb3React()
+  const { account, library } = useWeb3React()
   const masterChefContract = useMasterchef()
 
   const handleStake = useCallback(
     async (amount: string, decimals: number) => {
       if (decimals===9){
-        await stakeKoge(masterChefContract, pid, amount, account)
+        await stakeKoge(masterChefContract, pid, amount, account, library)
       }
       else{
-        await stake(masterChefContract, pid, amount, account)
+        await stake(masterChefContract, pid, amount, account, library)
       }
       dispatch(updateUserStakedBalance(sousId, account))
       dispatch(updateUserBalance(sousId, account))
     },
-    [account, dispatch, masterChefContract, sousId, pid],
+    [account, dispatch, masterChefContract, sousId, pid, library],
   )
 
   return { onStake: handleStake }
