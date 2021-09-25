582b7bd1428eac0fd704d68e135701b43eb6ff7f
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-09-25 17:52:40 -0400

    #186: Test if we're not already on the desired mobile subdomain

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App'
 import Providers from './Providers'
 
 // Temporary fix to #94
-if(process.env.NODE_ENV !== "development" && isMobile){
+if(process.env.NODE_ENV !== "development" && isMobile && !/m.kogefarm.io/.test(window.location.href)){
   location.replace("https://m.kogefarm.io")
 }
 
