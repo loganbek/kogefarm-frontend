98c178dd2a42c37364922049240abbe114111f19
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-10-02 20:14:01 -0400

    #198: Implemented NetworkSwitcher

---
+++ src/components/NetworkSwitcher.tsx
@@ -0,0 +1,42 @@
+import { CHAINS, SUPPORTED_CHAINS } from "config/index"
+import useAuth from "hooks/useAuth"
+import useNetworkSwitcher from "hooks/useNetworkSwitcher"
+import React from "react"
+import { setFarms } from "state/farms"
+import { useAppDispatch } from "state/index"
+import { setupNetwork } from "utils/wallet"
+import Select, { OptionProps } from "./Select/Select"
+
+
+function NetworkSwitcher() {
+    const dispatch = useAppDispatch()
+    const { getCurrentNetwork, setCurrentNetwork } = useNetworkSwitcher()
+    const { logout } = useAuth(getCurrentNetwork())
+
+    const networks = React.useMemo(
+        () => [
+            { ...CHAINS[SUPPORTED_CHAINS.MATIC], SUPPORTED_CHAIN: SUPPORTED_CHAINS.MATIC },
+            { ...CHAINS[SUPPORTED_CHAINS.MOONRIVER], SUPPORTED_CHAIN: SUPPORTED_CHAINS.MOONRIVER },
+        ]
+            .map(n => ({
+                label: n.chainNameAbbr,
+                value: n.SUPPORTED_CHAIN,
+                icon: <img key={n.logoUrl} src={n.logoUrl} alt={n.chainNameAbbr} style={{ width: 20, height: 20 }} />
+            }))
+        , [])
+
+    const [value, setValue] = React.useState(networks.find(v => v.value === getCurrentNetwork()))
+
+    const changeNetwork = (v: OptionProps) => {
+        dispatch(setFarms(CHAINS[v.value].farms ?? []))
+        setCurrentNetwork(v.value)
+        setValue(v)
+        logout()
+        setupNetwork(v.value)
+    }
+
+    return <Select value={value} options={networks} onChange={changeNetwork} />
+
+}
+
+export default React.memo(NetworkSwitcher)
