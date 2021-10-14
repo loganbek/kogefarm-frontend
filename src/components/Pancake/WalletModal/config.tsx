4c5ea6376fa43ee48073740749a219e697878911
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-10-13 20:20:56 -0400

    Fix #300: Dropped support to only Metamask and TrustWallet

--- src/components/Pancake/WalletModal/config.tsx
+++ src/components/Pancake/WalletModal/config.tsx
@@ -18,13 +18,13 @@ const connectors: Config[] = [
     bg: 'rgba(245, 132, 31, 0.2)'
 
   },
-  {
-    title: "WalletConnect",
-    icon: WalletConnect,
-    connectorId: ConnectorNames.WalletConnect,
-    priority: 2,
-    bg: 'rgba(59, 153, 252, 0.2)'
-  },
+  // {
+  //   title: "WalletConnect",
+  //   icon: WalletConnect,
+  //   connectorId: ConnectorNames.WalletConnect,
+  //   priority: 2,
+  //   bg: 'rgba(59, 153, 252, 0.2)'
+  // },
   {
     title: "Trust Wallet",
     icon: TrustWallet,
@@ -33,42 +33,42 @@ const connectors: Config[] = [
     bg: 'rgba(51, 117, 187, 0.2)'
 
   },
-  {
-    title: "MathWallet",
-    icon: MathWallet,
-    connectorId: ConnectorNames.Injected,
-    priority: 999,
-    bg: 'rgba(0, 0, 0, 0.2)'
-  },
-  {
-    title: "TokenPocket",
-    icon: TokenPocket,
-    connectorId: ConnectorNames.Injected,
-    priority: 999,
-    bg: 'rgba(0, 166, 242, 0.2)'
-  },
+  // {
+  //   title: "MathWallet",
+  //   icon: MathWallet,
+  //   connectorId: ConnectorNames.Injected,
+  //   priority: 999,
+  //   bg: 'rgba(0, 0, 0, 0.2)'
+  // },
+  // {
+  //   title: "TokenPocket",
+  //   icon: TokenPocket,
+  //   connectorId: ConnectorNames.Injected,
+  //   priority: 999,
+  //   bg: 'rgba(0, 166, 242, 0.2)'
+  // },
 
-  {
-    title: "Binance Chain",
-    icon: BinanceChain,
-    connectorId: ConnectorNames.BSC,
-    priority: 999,
-    bg: 'rgba(240, 185, 11, 0.2)'
-  },
-  {
-    title: "SafePal",
-    icon: SafePal,
-    connectorId: ConnectorNames.Injected,
-    priority: 999,
-    bg: 'rgba(0, 0, 0, 0.2)'
-  },
-  {
-    title: "Coin98",
-    icon: Coin98,
-    connectorId: ConnectorNames.Injected,
-    priority: 999,
-    bg: 'rgba(223, 190, 70, 0.2)'
-  },
+  // {
+  //   title: "Binance Chain",
+  //   icon: BinanceChain,
+  //   connectorId: ConnectorNames.BSC,
+  //   priority: 999,
+  //   bg: 'rgba(240, 185, 11, 0.2)'
+  // },
+  // {
+  //   title: "SafePal",
+  //   icon: SafePal,
+  //   connectorId: ConnectorNames.Injected,
+  //   priority: 999,
+  //   bg: 'rgba(0, 0, 0, 0.2)'
+  // },
+  // {
+  //   title: "Coin98",
+  //   icon: Coin98,
+  //   connectorId: ConnectorNames.Injected,
+  //   priority: 999,
+  //   bg: 'rgba(223, 190, 70, 0.2)'
+  // },
 ];
 
 export default connectors;
